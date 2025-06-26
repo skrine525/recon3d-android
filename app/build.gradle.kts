f2482f0816c8a68b62e7facdf447fc02918671f9
skrine525 <tsaregorodtsev03@mail.ru>
2025-06-27 06:36:30 +1000

    [WIP] refactor fullscreen progress

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.dvfu.diplom3d"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
