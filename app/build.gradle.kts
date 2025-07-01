c913d7e9ffdfef176c593b57ac04f62cf34a8981
skrine525 <tsaregorodtsev03@mail.ru>
2025-07-01 23:50:49 +1000

    [WIP]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.dvfu.diplom3d"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.0.6"
+        versionCode = 7
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
